87cff3c4a1717820a71a38186dfd92af25daf72e
Jun Park <junpark0909@gmail.com>
2023-03-10 16:06:11 -0500

    Modified main.py code + Added Google Spreadsheet Export Function

---
+++ google_spreadsheet.py
@@ -0,0 +1,79 @@
+"""
+Exporting Spotify Playlist Links from Survey Results
+"""
+from __future__ import print_function
+
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = "1aGhKcdd-hFxZggOMNydM2ElF2gU0IO6JkkWo3SN6RZA"
+RANGE_NAME = "C2:F"
+
+
+def googlesheet():
+    """
+    Return a dictionary of Spotify Playlist links according to the specifc year
+    the playlist was made
+
+    Args:
+        Any parameter
+
+    Returns:
+        A dictionary with years(2019,2020,2021,2022) as keys
+        and playlist links as values
+
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "client_secret_899143637035-ehhtn86j8tcglm9uhne3n3vc0d0kevbm.apps.googleusercontent.com.json",
+                SCOPES,
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w", encoding="utf-8") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+        )
+
+        # Categorize the playlists into their specific years into a library
+        shelf = {"2019": [], "2020": [], "2021": [], "2022": []}
+        for play in result["values"]:
+            if len(play) > 1:
+                shelf["2019"].append(play[0])
+                shelf["2020"].append(play[1])
+                shelf["2021"].append(play[2])
+                shelf["2022"].append(play[3])
+
+        return shelf
+
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    googlesheet()
--- main.py
+++ main.py
@@ -2,19 +2,21 @@
 Functions to determine the overall "mood" of a user's Spotify Wrapped playlist
 """
 import numpy
+import requests
 import spotipy
 import validators
-import credit
+import spotify_key
 
 TEST_URL = "https://open.spotify.com/playlist/1XbedDdXpoV8Q0YUQpoyDQ"
 
 client_credentials_manager = spotipy.SpotifyClientCredentials(
-    client_id=credit.SPOTIPY_CLIENT_ID, client_secret=credit.SPOTIPY_CLIENT_SECRET
+    client_id=spotify_key.SPOTIPY_CLIENT_ID,
+    client_secret=spotify_key.SPOTIPY_CLIENT_SECRET,
 )
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-def playlist_uri(link):
+def playlist_uri(playlist_link):
     """
     Return the Spotify URI from an input URL
 
@@ -25,15 +27,14 @@ def playlist_uri(link):
         A string representing the URI of a user's playlist
     """
     try:
-        if validators.url(link):
-            uri = link.split("/")[-1].split("?")[0]
+        if validators.url(playlist_link):
+            uri = playlist_link.split("/")[-1].split("?")[0]
             return uri
-        return print("Please enter a valid link. Try again")
-    except TypeError:
+    except spotipy.exceptions.SpotifyException:
         return print("Please enter a valid link. Try again")
 
 
-def get_track_id(link):
+def get_track_id(playlist_link):
     """
     Return a list of Spotify Track IDs from an input URL
 
@@ -43,12 +44,28 @@ def get_track_id(link):
     Returns:
         A list representing all the track IDs of a user's playlist
     """
-    track_uri = playlist_uri(link)
+    track_uri = playlist_uri(playlist_link)
     track_id = [x["track"]["id"] for x in sp.playlist_tracks(track_uri)["items"]]
     return track_id
 
 
-def get_valence(link):
+def get_username(playlist_link):
+    """
+    Return the name of the owner of a playlist
+
+    Args:
+        link: A string representing the URL of a user's playlist
+
+    Returns:
+        A string representing the name of the owner
+
+    """
+    name = sp.playlist_items(playlist_link)["items"][0]["added_by"]["id"]
+    name = name.capitalize()
+    return name
+
+
+def get_valence(playlist_link):
     """
     Return a list of "valence" values for each track in a user's playlust
 
@@ -58,14 +75,14 @@ def get_valence(link):
 
         A list of integers representing all the valence values from a user's playlist
     """
-    track_id = get_track_id(link)
+    track_id = get_track_id(playlist_link)
     valence = []
     for ids in track_id:
         valence.append(sp.audio_features(ids)[0]["valence"])
     return valence
 
 
-def average_valence(link):
+def average_valence(playlist_link):
     """
     Return an integer that represents how happy a user was
     Based on a user's Spotify Wrapped playlist
@@ -79,24 +96,33 @@ def average_valence(link):
     Returns:
         An integer representing the average valence of a playlist
     """
-    all_valence = get_valence(link)
+    all_valence = get_valence(playlist_link)
     return numpy.mean(all_valence)
 
 
-def how_happy(link):
+def how_happy_is(playlist_link):
     """
     Return a message telling how happy a user was that year
     based on a user's Spotify Wrapped playlist
 
+    This function is for users who individually requested their
+    playlist results to be reported back
+
     Args:
-        link: A string representing the URL of a user's playlist
+        link: A string representing a user's name
 
     Returns:
-        A string that is message of how happy a user was
+        A string that is a message of how happy a user was
     """
-    happiness_meter = numpy.round(average_valence(link), decimals=2)
+    happiness_meter = numpy.round(average_valence(playlist_link), decimals=2)
+    name = get_username(playlist_link)
     year = input("Enter when this playlist was made: ")
-    return print(f"You were about {happiness_meter} happy out of 1 in {year}!")
+    return print(
+        f"{name} was about {happiness_meter} happy out of 1 in the Year {year}!"
+    )
 
 
-how_happy(input("Enter your Spotify Playlist Link: "))
+# link = input("Enter your Spotify Playlist Link: ")
+
+
+# how_happy_is(link)
